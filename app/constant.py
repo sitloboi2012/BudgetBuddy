83586a78f4b74ad4da6ec3a9bccbe097c7d04b4c
phuchauxd12 <118866081+phuchauxd12@users.noreply.github.com>
2023-12-02 19:47:42 +0700

    done register page,router and test register page

--- app/constant.py
+++ app/constant.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-from enum import StrEnum
+from enum import Enum
 from pydantic import BaseModel
 
 import os
 
 
-class Constant(StrEnum):
+class Constant(Enum):
     MONGODB_URI = os.environ["MONGODB_URI"]
 
 
--- app/routers/register.py
+++ app/routers/register.py
@@ -2,20 +2,41 @@
 from fastapi import APIRouter, Form
 from fastapi.responses import JSONResponse
 from models.users import UserInfo
-from constant import Constant
+from models.router_response import Message
+# from constant import Constant
 from pymongo import MongoClient
+import bcrypt
+
 
 router = APIRouter(prefix="/api/v1", tags=["User Register"])
-client = MongoClient(host=Constant.MONGODB_URI).get_database("dev")
+client = MongoClient(host="mongodb+srv://phuchauxd12:Abcd0123@cluster0.lf8sh9p.mongodb.net/").get_database("dev")
 db = client.get_collection("USERS")
 
 
-@router.post("/register")
+@router.post("/register", responses= {409: {"model": Message},
+                                      422: {"model": Message}})
 def register_user(
     user_name: str = Form(..., description="Username of the user"),
     password: str = Form(..., description="Password of the user"),
     full_name: str = Form(..., description="Full name of the user"),
+    number: str = Form(None, description="Phone number of the user"),
+    email: str = Form(..., description="Email address of the user"),
+    address: str = Form(None, description="Address of the user")
 ):
-    db.insert_one(
-        UserInfo(username=user_name, full_name=full_name, password=password).dict()
-    )
+    password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    if db.find_one({'username': user_name}):
+        return JSONResponse(status_code=409, content={"message": "Username already exists"})
+    if db.find_one({'email': email}):
+        return JSONResponse(status_code=409, content={"message": "Email already exists"})
+    try: 
+        db.insert_one(
+            UserInfo(username=user_name, full_name=full_name, password=password, number=number, email= email, address= address).dict())
+    except ValueError as e:
+        # Handle Pydantic validation errors
+        return JSONResponse(status_code=422, content={"message": str(e)}) # error for email and number
+    
+    assert db.find_one({'username': user_name})['username'] == user_name # test case 
+    assert len(list(db.find({'username': user_name}))) == 1    # test case
+
+    return JSONResponse(content={"user_name": user_name, "password": password.decode('utf-8'), "full_name": full_name, "number": number, "email": email, "address": address})
+
