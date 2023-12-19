1059693e4fa55e7a277181fab3d17491836adbc2
nskadk <118862276+nskadk@users.noreply.github.com>
2023-12-19 10:03:40 +0700

    routing

--- client/electron_vue/src/renderer/App.vue
+++ client/electron_vue/src/renderer/App.vue
@@ -1,22 +1,45 @@
 <script setup lang="ts">
-import Networth from './components/Networth.vue';
-import OverviewVue from './components/Overview.vue';
+import { ref, computed, DefineComponent, ComponentOptionsMixin, ExtractPropTypes } from 'vue'
+import Home from './views/Homepage.vue' 
+import Plan from './views/Plan.vue'
 window.electronAPI.sendMessage('Hello from App.vue!');
+
+type PublicProps = {
+  title?: string;
+  description?: string;
+};
+
+interface RouteDefinition {
+  [path: string]: DefineComponent<{}, {}, {}, {}, {}, ComponentOptionsMixin, ComponentOptionsMixin, {}, string, PublicProps, Readonly<ExtractPropTypes<{}>>, {}>;
+}
+
+const routes: RouteDefinition = {
+  '/home': Home,
+  '/plan': Plan
+}
+
+const currentPath = ref(window.location.hash)
+
+window.addEventListener('hashchange', () => {
+  currentPath.value = window.location.hash
+})
+
+const currentView = computed(() => {
+  return routes[currentPath.value.slice(1) || '/'];
+});
 </script>
 
 <template>
   <div class="homebar">
     <ul>
-        <li><a href="#overview">Overview</a></li>
+        <li><a href="#/home">Overview</a></li>
         <li><a href="#transaction">Transaction</a></li>
         <li><a href="#investment">Investment</a></li>
-        <li><a href="#plan">Plan</a></li>
+        <li><a href="#/plan">Plan</a></li>
         <li><a href="#report">Report</a></li>
     </ul>
   </div>
-  <section>
-    <div><Networth/></div>
-    <div class="screen"><OverviewVue/></div>
-  </section>
+  <component :is="currentView" />
 </template>
+
 
---
+++ client/electron_vue/src/renderer/views/Plan.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Networth from '../components/Networth.vue';
+import OverviewVue from '../components/Overview.vue'; 
+</script>
+
+<template>
+   <section class="display">
+    <div><Networth/></div>
+    <div class="screen"><OverviewVue/></div>
+  </section> 
+</template>
