40078001af316633cdf6d4b5eb8389019c3d7945
huyvo6812 <huyvo6812@gmail.com>
2023-12-25 10:11:17 +0700

    update goal api route

--- app/models/goal.py
+++ app/models/goal.py
@@ -2,23 +2,38 @@
 from __future__ import annotations
 
 from pydantic import BaseModel, Field
-from datetime import datetime
 from bson import ObjectId
 
 
 class GoalSettingBaseModel(BaseModel):
-    goal_created_date: datetime.date = Field(..., description="Goal created date")
-    goal_end_date: datetime.date = Field(..., description="Goal end date")
+    goal_created_date: str = Field(..., description="Goal created date")
+    goal_end_date: str = Field(..., description="Goal end date")
     goal_name: str = Field(..., description="Goal name")
+    account_id: ObjectId = Field(..., description="Account id")
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
 
 
-class InvestmentGoalModel(GoalSettingBaseModel):
+class InvestmentGoalModel(BaseModel):
     goal_id: ObjectId = Field(..., description="Goal id")
     investment_account_id: ObjectId = Field(..., description="Investment account id")
     current_profit: float = Field(..., description="Current profit")
 
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
 
-class SavingGoalModel(GoalSettingBaseModel):
+
+class SavingGoalModel(BaseModel):
     goal_id: ObjectId = Field(..., description="Goal id")
     saving_account_id: ObjectId = Field(..., description="Saving account id")
     saving_goal_value: float = Field(..., description="Saving goal value")
+
+    class Config:
+        allow_population_by_field_name = True
+        arbitrary_types_allowed = True
+        json_encoders = {ObjectId: str}
--- app/routers/goal.py
+++ app/routers/goal.py
@@ -3,22 +3,65 @@ from __future__ import annotations
 
 from fastapi import APIRouter, Form, HTTPException
 from fastapi.responses import JSONResponse
-from constant import Message, Constant
+from constant import Constant
 from pymongo import MongoClient
 from bson import ObjectId
+from datetime import datetime
+from models.goal import InvestmentGoalModel, SavingGoalModel, GoalSettingBaseModel
 
 router = APIRouter(prefix="/api/v1", tags=["Goal Setting"])
 client = MongoClient(host=Constant.MONGODB_URI).get_database("dev")
-saving_goal_db = client.get_collection("SAVING_GOAL_SETTINGS")
-investment_goal_db = client.get_collection("INVESTMENT_GOAL_SETTINGS")
-goal_overall_db = client.get_collection("GOAL_OVERALL_SETTINGS")
+SAVING_GOAL_DB = client.get_collection("SAVING_GOAL_SETTINGS")
+INVESTMENT_GOAL_DB = client.get_collection("INVESTMENT_GOAL_SETTINGS")
+GOAL_SETTING_BASE = client.get_collection("GOAL_SETTINGS")
+
+# MODEL INITIALIZATION
+MODEL = {
+    "Saving": [SavingGoalModel, SAVING_GOAL_DB],
+    "Investment": [InvestmentGoalModel, INVESTMENT_GOAL_DB],
+}
 
 
 @router.post("/goal_setting/{user_id}/{account_id}")
 def set_goal(
-    user_id: ObjectId,
-    account_id: ObjectId,
+    account_id: str,
+    account_type: str = Form(..., description="Account type"),
     goal_end_date: str = Form(..., description="Goal end date"),
+    goal_created_date: str = Form(..., description="Goal created date"),
     goal_name: str = Form(..., description="Goal name"),
-):
-    pass
+) -> JSONResponse:
+    start_date = datetime.strptime(goal_created_date, "%Y-%m-%d").date()
+    end_date = datetime.strptime(goal_end_date, "%Y-%m-%d").date()
+
+    if start_date < datetime.now().date():
+        raise HTTPException(status_code=400, detail="Start date cannot be in the past")
+
+    if end_date < start_date:
+        raise HTTPException(
+            status_code=400, detail="End date must be greater than start date"
+        )
+
+    GOAL_SETTING_BASE.insert_one(
+        GoalSettingBaseModel(
+            goal_created_date=goal_created_date,
+            goal_end_date=goal_end_date,
+            goal_name=goal_name,
+            account_id=ObjectId(account_id),
+        ).dict()
+    )
+
+    goal_id = GOAL_SETTING_BASE.find_one(
+        {"goal_name": goal_name, "account_id": ObjectId(account_id)}
+    )["_id"]
+
+    print(goal_id)
+    """
+    if account_type == "Saving":
+        SAVING_GOAL_DB.insert_one(
+            SavingGoalModel(
+                goal_id=ObjectId(account_id),
+                saving_account_id=ObjectId(account_id),
+                saving_goal_value=0,
+            ).dict()
+        )
+    """
