ca717233f29cd1336212275fa2a2d45e9ed52538
47Nguyen <ankhanhthinj@gmail.com>
2023-12-31 15:50:57 +0700

    Edited API and requirements text

--- app/routers/prediction.py
+++ app/routers/prediction.py
@@ -12,13 +12,14 @@ from pymongo import MongoClient
 from skforecast.ForecasterAutoreg import ForecasterAutoreg
 warnings.filterwarnings('ignore')
 
-
+# Connect to DB
 router = APIRouter(prefix="/api/v1", tags=["Income Prediction"])
 client = MongoClient(host=Constant.MONGODB_URI).get_database("dev")
 db = client.get_collection("TRANSACTION_HISTORY")
 
+      
 @router.get('/prediction/{user_id}')
-def income_prediction(past: int, future: int, user_id:str):
+def income_prediction(user_id:str):
     list_transaction = db.find({"user_id": ObjectId(user_id)})
     array = [
            GetTransactionInformation(
@@ -33,20 +34,22 @@ def income_prediction(past: int, future: int, user_id:str):
             ).dict()
             for value in list_transaction
         ]
-    """ This takes the user ID, as every user has their own ID and 
-    transaction. By inputting a user id, it filters out the transaction history
-    and only display the transaction to its users."""
     if not array:
         return JSONResponse(status_code=404, content={'message': "Transaction does not exist."})
         
-    def recursive_prediction(d_past, d_future ):
-        df = pd.DataFrame(array)
-        headers = ['transaction_date', 'Amount']
-        df = df[headers]
-        df['transaction_date'] = pd.to_datetime(df['transaction_date'], dayfirst=True)
-        prediction = ForecasterAutoreg(regressor = RandomForestRegressor(), lags = d_past)
-        prediction.fit(y = df['Amount'])
-        forecasting = jsonable_encoder(prediction.predict(steps=d_future).to_dict())
-        return forecasting
-    
-    return JSONResponse(content=recursive_prediction(past, future))
+    def recursive_prediction(d_future):
+            df = pd.DataFrame(array)
+            headers = ['transaction_date', 'Amount']    
+            df = df[headers]
+            df['transaction_date'] = pd.to_datetime(df['transaction_date'], dayfirst=True)
+            past =(len(df['transaction_date'].unique())-1)
+
+            """ Past will fetch all the history transaction date of the user,
+            combined, all the dates that has the same day, into one. We then use that data to make prediction
+            for the upcoming 6 months."""
+            prediction = ForecasterAutoreg(regressor = RandomForestRegressor(), lags = past)
+            prediction.fit(y = df['Amount'])
+            forecasting = jsonable_encoder(prediction.predict(steps=d_future).to_dict())
+            return forecasting
+
+    return JSONResponse(content=recursive_prediction(180))
